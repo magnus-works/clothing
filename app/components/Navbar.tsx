af9676b86f34c9e8d27e2ed458edb65338dc25b7
vishnuvirat <vishnuviratkohli5@gmail.com>
2024-07-03 18:10:17 +0530

    Updated the side menu bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,13 +3,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {
-    ArrowRight,
-    BadgeIndianRupee,
-    ShoppingBag,
-    ShoppingCart,
-} from "lucide-react";
-import { useShoppingCart } from "use-shopping-cart";
+import { ArrowRight, BadgeIndianRupee, ShoppingCart } from "lucide-react";
 import { CalendarDays, Search, Heart, User } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -22,16 +16,6 @@ import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 
 import { CircleUser, Menu, Package2 } from "lucide-react";
-
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -46,19 +30,25 @@ import {
     SheetContent,
     SheetTrigger,
 } from "@/components/ui/sheet";
-import React, { useState } from "react";
-import { HoverBorderGradient } from "../../components/ui/hover-border-gradient";
 import { AnimatedButton } from "../../components/ui/moving-border";
+import image1 from "../../public/MenuPhotos/Home.jpg";
+import image2 from "../../public/MenuPhotos/MenMenu.jpg";
+import image3 from "../../public/MenuPhotos/Women.jpg";
+import image4 from "../../public/MenuPhotos/Kids.jpg";
+import Image from "next/image";
 const links = [
-    { name: "Home", href: "/" },
-    { name: "Men", href: "/Men" },
-    { name: "Women", href: "/Women" },
-    { name: "Kids", href: "/Kids" },
+    { name: "Home", href: "/", img: image1 },
+    { name: "Men", href: "/Men", img: image2 },
+    {
+        name: "Women",
+        href: "/Women",
+        img: image3,
+    },
+    { name: "Kids", href: "/Kids", img: image4 },
 ];
 
 export default function Navbar() {
     const pathname = usePathname();
-    // const { handleCartClick } = useShoppingCart();
     return (
         <div>
             <div className="flex justify-center bg-primary h-10 md:gap-48 lg:gap-96">
@@ -111,17 +101,6 @@ export default function Navbar() {
             </div>
             <div className="flex p-2 mx-10 border-b border-gray-200">
                 <div className="flex-1 justify-center items-center">
-                    {/* <div className="flex rounded-full border border-gray-200 w-[370px] h-12 items-center p-2">
-                        <Input
-                            type="search"
-                            placeholder="I'm Looking For..."
-                            className="border-hidden outline-none ring-0"
-                        />
-                        <div className="rounded-full bg-green-100 h-10   w-10 flex items-center justify-center mr-2">
-                            <Search className="h-4 w-4 text-green-600" />
-                        </div>
-                    </div> */}
-
                     <AnimatedButton
                         borderRadius="1.75rem"
                         containerClassName="w-60"
@@ -212,11 +191,21 @@ export default function Navbar() {
                                             Acme Inc
                                         </span>
                                     </Link>
+
                                     {links.map((link, idx) => (
                                         <div
-                                            className="flex border-b border-gray-200 justify-between"
+                                            className="flex border-b border-gray-200 justify-center items-center"
                                             key={idx}
                                         >
+                                            <div className="mx-4 mb-1">
+                                                <Image
+                                                    className="rounded-full"
+                                                    src={link.img}
+                                                    width={30}
+                                                    alt=""
+                                                    height={30}
+                                                />
+                                            </div>
                                             {pathname === link.href ? (
                                                 <SheetClose asChild>
                                                     <Link
@@ -236,7 +225,7 @@ export default function Navbar() {
                                                     </Link>
                                                 </SheetClose>
                                             )}
-                                            <div>
+                                            <div className="flex flex-1 justify-end">
                                                 <ArrowRight />
                                             </div>
                                         </div>
@@ -286,49 +275,5 @@ export default function Navbar() {
                 </div>
             </div>
         </div>
-        // <header className="mb-8 border-b">
-        //     <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
-        //         <Link href="/">
-        // <h1 className="text-2xl md:text-4xl font-bold">
-        //     Next<span className="text-primary">Commerce</span>
-        // </h1>
-        //         </Link>
-
-        //         <nav className="hidden gap-12 lg:flex 2xl:ml-16">
-        // {links.map((link, idx) => (
-        //     <div key={idx}>
-        //         {pathname === link.href ? (
-        //             <Link
-        //                 className="text-lg font-semibold text-primary"
-        //                 href={link.href}
-        //             >
-        //                 {link.name}
-        //             </Link>
-        //         ) : (
-        //             <Link
-        //                 href={link.href}
-        //                 className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary"
-        //             >
-        //                 {link.name}
-        //             </Link>
-        //         )}
-        //     </div>
-        // ))}
-        //         </nav>
-
-        //         <div className="flex divide-x border-r sm:border-l">
-        //             <Button
-        //                 variant={"outline"}
-        //                 // onClick={() => handleCartClick()}
-        //                 className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
-        //             >
-        //                 <ShoppingBag />
-        //                 <span className="hidden text-xs font-semibold text-gray-500 sm:block">
-        //                     Cart
-        //                 </span>
-        //             </Button>
-        //         </div>
-        //     </div>
-        // </header>
     );
 }
