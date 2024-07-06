84bc47e523549eff1a2f9efdb6829e6d34e8ff42
vishnuvirat <vishnuviratkohli5@gmail.com>
2024-07-06 13:10:14 +0530

    updated manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="manifest" href="/manifest.json" />
+            </head>
             <body className={inter.className}>
                 <Navbar />
                 <Toaster />
